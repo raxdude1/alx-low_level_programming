ae563f35962d5af118f4fbc394b2e62e8ccb9e99
raxdude1 <125977688+raxdude1@users.noreply.github.com>
2023-03-20 15:31:09 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: 1
  */
-int main(void)
 {
-fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	      return (1);
 }
